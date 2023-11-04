e0610a409eb13986f91280a491c4148b10ed10a3
leowrites <75044178+leowrites@users.noreply.github.com>
2023-11-03 22:16:42 -0400

    add basic pet listing model

--- P2/shelters/models/application_response.py
+++ P2/shelters/models/application_response.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .pet_application import PetApplication
+from .pet_application import PetApplication, PetListing
 
 
 # each question is owned by a shelter
@@ -11,10 +11,13 @@ class Question(models.Model):
 # one question can be used for many listings, one listing can have many questions
 class ListingQuestion(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    # listing = models.ForeignKey(PetListing, on_delete=models.CASCADE)
+    listing = models.ForeignKey(PetListing, related_name='listing_questions', on_delete=models.CASCADE)
 
 
 class Answer(models.Model):
     answer = models.CharField(max_length=3000)  # 3000 chars is approx 500 words
     question = models.ForeignKey(ListingQuestion, on_delete=models.CASCADE)
-    application = models.ForeignKey(PetApplication, on_delete=models.CASCADE)
+    application = models.ForeignKey(PetApplication, related_name='answers', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.question}: {self.answer}'
