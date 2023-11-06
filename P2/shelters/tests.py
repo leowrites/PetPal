4e0b576a53ca6429f095393758172bb2bf7695b2
leowrites <75044178+leowrites@users.noreply.github.com>
2023-11-06 17:37:57 -0500

    add tests for user and shelter creation

--- P2/shelters/tests.py
+++ P2/shelters/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
-
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from users.views import views
+from shelters.models import Shelter
 # Create your tests here.
+
+
+class UserCreationTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_CreateUser_UserDoesNotExist_CreatesUser(self):
+        request = self.factory.post('/users', {
+            'username': 'Leo',
+            'password': '123123123a!',
+        })
+        views.CreateOrListUsers.as_view()(request)
+        user = User.objects.get(username='Leo')
+        self.assertEquals(user.username, 'Leo')
+
+    def test_CreateUser_userExists_UserExistsException(self):
+        request = self.factory.post('/users', {
+            'username': 'Leo',
+            'password': '123123123a!',
+        })
+        response = views.CreateOrListUsers.as_view()(request)
+        self.assertEquals(response.status_code, 201)
+        response = views.CreateOrListUsers.as_view()(request)
+        self.assertEquals(response.status_code, 400)
+        self.assertEquals(response.data['username'][0], 'A user with that username already exists.')
+
+
+class ShelterUserCreationTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+    def test_CreateUser_UserIsShelter_CreatesUserAndShelter(self):
+        request = self.factory.post('/users', {
+            'username': 'Leo',
+            'password': '123123123a!',
+            'is_shelter': True
+        })
+        views.CreateOrListUsers.as_view()(request)
+        user = User.objects.get(username='Leo')
+        shelter = Shelter.objects.get(owner=user)
+        self.assertEquals(user.username, 'Leo')
+        self.assertEquals(shelter.owner, user)
+
+    def test_CreateUser_UserIsNotShelter_CreatesUserOnly(self):
+        request = self.factory.post('/users', {
+            'username': 'Leo',
+            'password': '123123123a!'
+        })
+        views.CreateOrListUsers.as_view()(request)
+        user = User.objects.get(username='Leo')
+        with self.assertRaises(Shelter.DoesNotExist):
+            Shelter.objects.get(owner=user)
