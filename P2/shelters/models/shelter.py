6fce418d9866a9de907d60e6143147f13736c9d4
leowrites <75044178+leowrites@users.noreply.github.com>
2023-11-05 21:34:21 -0500

    allow user to declare that they are a shelter

--- P2/shelters/models/shelter.py
+++ P2/shelters/models/shelter.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Shelter(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='')
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
--- P2/users/serializers/serializers.py
+++ P2/users/serializers/serializers.py
@@ -1,18 +1,26 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
+from shelters.models.shelter import Shelter
 
 
 class UserSerializer(serializers.ModelSerializer):
+    is_shelter = serializers.BooleanField(label="Are you a shelter?", write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'username', 'password', 'is_shelter']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        # create a shelter if user is a shelter
+        is_shelter = validated_data.pop('is_shelter', False)
+        user = User.objects.create_user(**validated_data)
+        if is_shelter:
+            Shelter.objects.create(owner=user)
+        return user
 
     def validate_password(self, password):
         validate_password(password=password)
