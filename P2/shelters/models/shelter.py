e11f6ad747881cf75055ae242cc161a33e356578
justinwli930 <justinwli930@gmail.com>
2023-11-14 18:32:36 -0500

    new migration to remove defaults

--- P2/shelters/models/shelter.py
+++ P2/shelters/models/shelter.py
@@ -4,6 +4,6 @@ from users.models import User
 class Shelter(models.Model):
     shelter_name = models.CharField(max_length=200, default='')
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    contact_email = models.EmailField(default='temp@temporaryfix.com') # TODO: fix when we start db from scratch
-    location = models.CharField(max_length=200, default='tempLocation') # TODO: fix when we start db from scratch
-    mission_statement = models.TextField(default='tempMissionStatement') # TODO: fix when we start db from scratch
+    contact_email = models.EmailField()
+    location = models.CharField(max_length=200)
+    mission_statement = models.TextField() 
