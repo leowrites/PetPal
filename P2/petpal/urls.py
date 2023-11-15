47883da2fcc06590fd86df8c70874c3110efd9a8
justinwli930 <justinwli930@gmail.com>
2023-11-14 23:58:06 -0500

    fixed images not showing up

--- P2/petpal/urls.py
+++ P2/petpal/urls.py
@@ -19,6 +19,9 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -28,3 +31,6 @@ urlpatterns = [
     path('shelters/', include('shelters.urls', namespace='shelters')),
     path('notifications/', include('notifications.urls', namespace='notifications')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- P2/users/urls.py
+++ P2/users/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 app_name='users'
 urlpatterns = [
     path('', views.CreateUser.as_view(), name='user-list-create'),
     path('<int:pk>', views.RetrieveOrUpdateOrDestroyUser.as_view(), name='user-details'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
