d81e88eaad4749a41cf3bde3af8c68d1eab0ebee
Leo <75044178+leowrites@users.noreply.github.com>
2023-11-15 09:15:42 -0500

    update scripts (#126)

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,6 +1,8 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install virtualenv
-python3 -m virtualenv venv --python=python3.9
+python3 -m virtualenv venv
+. venv/bin/activate
 python3 -m pip install -r requirements.txt
+make
 python3 manage.py makemigrations
 python3 manage.py migrate
