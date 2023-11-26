7b1d992572436d6050c0b23e92086bd74b85d8b2
Pierre-William Lessard <lessardpw@gmail.com>
2023-11-26 17:41:34 -0500

    nav bar

--- P3/frontend/src/pages/landing.js
+++ P3/frontend/src/pages/landing.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { NavBar } from '../components/navigation/NavBar';
 
 export const Landing = () => {
   return (
+    
     <div>
+      <NavBar />
       Landing
     </div>
   );
