f86da455197ca6bc0084f6d757d60d99fbc53967
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-11-26 18:28:04 -0500

    add: cors middleware to backend

--- P3/backend/petpal/settings.py
+++ P3/backend/petpal/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django_filters',
     'notifications',
     'listings',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React app's address
 ]
 
 ROOT_URLCONF = 'petpal.urls'
