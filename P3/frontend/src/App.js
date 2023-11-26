200230f7dd758c57d3e0660977c2beb0b76c1055
leowrites <leo@liuwork.ca>
2023-11-26 18:22:38 -0500

    fix routing

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Landing } from './pages/landing'
 import { Search } from './pages/search'
 import Layout from './components/layout/Layout';
+import PetDetail from './pages/PetDetail';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Layout />} >
+          <Route path="" element={<Landing />} />
+          <Route path="listings">
+            <Route path=":petId" element={<PetDetail />} />
+          </Route>
+          <Route path="search" element={<Search />} />
         </Route>
       </Routes>
     </Router>
