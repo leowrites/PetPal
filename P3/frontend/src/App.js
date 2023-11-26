a8b2bce163a0231cb65350b818bfabcea6eee993
Leo <75044178+leowrites@users.noreply.github.com>
2023-11-26 16:52:23 -0500

    add layout (#148)

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -3,13 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Landing } from './pages/landing'
 import { Search } from './pages/search'
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/search" element={<Search />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/search" element={<Search />} />
+        </Route>
       </Routes>
     </Router>
   );
