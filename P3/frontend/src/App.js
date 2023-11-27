76c06d90ec05d4899d476183db4108a1f52b6d53
Jason Wang <jasonwang0610@gmail.com>
2023-11-26 19:00:52 -0500

    152 add api layer and auth service (#158)
    * add: cors middleware to backend
    
    * add: api layer and auth to frontend

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -1,27 +1,36 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Landing } from './pages/landing'
 import { Search } from './pages/search'
 import Layout from './components/layout/Layout';
-import { Login } from './pages/Login'
-import { Signup } from './pages/Signup'
+import { setAuthToken } from './services/ApiService';
+import { Login } from './pages/login'
+import { Signup } from './pages/signup'
 import NotFound from './pages/NotFound'
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path='*' element={<NotFound />}/>
-        </Route>
-      </Routes>
-    </Router>
-  );
+    
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (token) {
+            setAuthToken(token)
+        }
+    }, [])
+        
+    return (
+        <Router>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path='*' element={<NotFound />}/>
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
