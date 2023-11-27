a6672656b94eb62a0ef232e813a185f86f627a72
leowrites <leo@liuwork.ca>
2023-11-26 20:57:13 -0500

    make listing standalone

--- P3/backend/listings/urls.py
+++ P3/backend/listings/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import ListPetListing
+from .views import (ListPetListing, ListOrCreateAssignedQuestion, RetrieveUpdateOrDestroyAssignedQuestion,
+                    RetrieveUpdateOrDeletePetListing, ListOrCreateApplicationForListing)
 
 app_name = "listings"
 
 urlpatterns = [
     path("", ListPetListing.as_view(), name="pet-listing-list"),
+    path('<int:listing_id>', RetrieveUpdateOrDeletePetListing.as_view(),
+         name='pet-listing-update-destroy'),
+    path('<int:listing_id>/questions', ListOrCreateAssignedQuestion.as_view(),
+         name='assigned-question-list-create'),
+    path('<int:listing_id>/questions/<int:question_id>',
+         RetrieveUpdateOrDestroyAssignedQuestion.as_view(), name='assigned-question-delete'),
+    path('<int:listing_id>/applications', ListOrCreateApplicationForListing.as_view(),
+         name='pet-application-list-create'),
 ]
