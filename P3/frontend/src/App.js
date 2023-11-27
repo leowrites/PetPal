07605fca372144691520b6ad05f7e8d4eecd2c9e
Leo <75044178+leowrites@users.noreply.github.com>
2023-11-26 19:19:11 -0500

    145 add pet details page (#156)
    * fix routing
    
    * fix import
    
    * fix footer not sticking to the bottom
    
    * add pet detail page
    
    * add placeholder asset
    
    * fix import
    
    * add error handling
    
    * add set auth token

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -4,33 +4,37 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Landing } from './pages/landing'
 import { Search } from './pages/search'
 import Layout from './components/layout/Layout';
+import PetDetail from './pages/PetDetail';
 import { setAuthToken } from './services/ApiService';
-import { Login } from './pages/login'
-import { Signup } from './pages/signup'
+// import { Login } from './pages/login'
+// import { Signup } from './pages/signup'
 import NotFound from './pages/NotFound'
 
 function App() {
-    
-    useEffect(() => {
-        const token = localStorage.getItem("token")
-        if (token) {
-            setAuthToken(token)
-        }
-    }, [])
-        
-    return (
-        <Router>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/search" element={<Search />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path='*' element={<NotFound />}/>
-                </Route>
-            </Routes>
-        </Router>
-    );
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      setAuthToken(token)
+    }
+  }, [])
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route path="" element={<Landing />} />
+          <Route path="listings">
+            <Route path=":petId" element={<PetDetail />} />
+          </Route>
+          <Route path="search" element={<Search />} />
+          {/* <Route path="login" element={<Login />} /> */}
+          {/* <Route path="signup" element={<Signup />} /> */}
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
