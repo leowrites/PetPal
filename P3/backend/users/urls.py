f54678c8bc81d097088a872f84397e6032671932
leowrites <leo@liuwork.ca>
2023-11-30 21:14:59 -0500

    rename url

--- P3/backend/users/urls.py
+++ P3/backend/users/urls.py
@@ -3,6 +3,6 @@ from .views import views
 
 app_name='users'
 urlpatterns = [
-    path('', views.RetrieveSelfOrCreateUser.as_view(), name='user-list-create'),
+    path('', views.RetrieveSelfOrCreateUser.as_view(), name='user-get-create'),
     path('<int:pk>', views.RetrieveOrUpdateOrDestroyUser.as_view(), name='user-details'),
 ]
