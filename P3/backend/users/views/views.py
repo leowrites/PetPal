0f09e5eb202fe445f06544363421b33ab5c15b33
leowrites <leo@liuwork.ca>
2023-11-30 21:14:07 -0500

    refactor

--- P3/backend/users/views/views.py
+++ P3/backend/users/views/views.py
@@ -3,36 +3,23 @@ from rest_framework import generics, permissions, mixins
 from users.serializers import serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.mixins import RetrieveModelMixin
 from users.permissions import permissions
 from django.shortcuts import get_object_or_404
 
+
 # GET /users/
 # POST /users/
-class RetrieveSelfOrCreateUser(generics.CreateAPIView):
-    permission_classes = [permissions.IsNotAuthenticated]
+class RetrieveSelfOrCreateUser(generics.RetrieveAPIView, generics.CreateAPIView):
     serializer_class = serializers.UserCreationSerializer
-    queryset = User.objects.all()
 
     def get_permissions(self):
         if self.request.method == "POST":
-            permission_classes = [permissions.IsNotAuthenticated]
-        elif self.request.method == "GET":
-            permission_classes = [permissions.IsAuthenticated]
-        return [permission() for permission in permission_classes]
+            return [permissions.IsNotAuthenticated()]
+        return [permissions.IsAuthenticated()]
 
-    def get_serializer_class(self):
-        if self.request.method == "POST":
-            return serializers.UserCreationSerializer
-        elif self.request.method == "GET":
-            return serializers.UserProfileSerializer
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        serializer = self.get_serializer(request.user)
-        return Response(serializer.data)
-
+    def get_object(self):
+        return self.request.user
 
 
 # GET /users/<pk>
