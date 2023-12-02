83aca8258a385b22f09062921961b184759e09ac
Leo <75044178+leowrites@users.noreply.github.com>
2023-12-01 20:31:39 -0500

    169 allow shelters to create custom questions (#170)
    * add route
    
    * extract overview box
    
    * add layout
    
    * update class to className
    
    * add services
    
    * use service call
    
    * add pet application form
    
    * send back information about shelter
    
    * allow type to be customized
    
    * add option to disable
    
    * allow user to apply for a pet
    
    * add listing detail when retrieving pet application
    
    * allow user to view read-only application
    
    * add tab for shelter message
    
    * move list create question to question app
    
    * set up routing for shelter question page
    
    * Add material tailwind and icon package
    
    * add create question
    
    * add modal
    
    * allow more options
    
    * add list and create
    
    * Delete P3/frontend/src/pages/ShelterQuestion/components/QuestionCard.jsx
    
    * Delete P3/frontend/src/pages/ShelterQuestion/ShelterQuestionPage.jsx
    
    * add ability to delete
    
    * improve UI
    
    * extract options
    
    * allow user to update a question
    
    * reformat
    
    * extend functionality to allow multiple questions be assigned at once
    
    * fix routing
    
    * add react loading skeleton
    
    * add colors constant
    
    * add skeleton to improve ux
    
    * add skeleton for loading
    
    * fix param
    
    * fix duplicated border
    
    * redirect user to login if they are not logged in
    
    * reformat file
    
    * set page to loading
    
    * remove redundant checks
    
    * protect routes

--- P3/backend/questions/views.py
+++ P3/backend/questions/views.py
@@ -6,11 +6,27 @@ from shelters.serializers import serializers
 from shelters.permissions import permissions
 
 
+class ListOrCreateShelterQuestion(generics.ListCreateAPIView):
+    serializer_class = serializers.ShelterQuestionSerializer
+    # only the owner of this shelter can get questions for this shelter, as well as make new ones
+    permission_classes = [IsAuthenticated, permissions.IsAnyShelterOwner, permissions.IsShelterOwner]
+
+    def get_queryset(self):
+        shelter = self.request.user.shelter
+        self.check_object_permissions(self.request, shelter)
+        return ShelterQuestion.objects.filter(shelter=shelter)
+
+    def perform_create(self, serializer):
+        shelter = self.request.user.shelter
+        self.check_object_permissions(self.request, shelter)
+        serializer.save(user=self.request.user)
+
+
 class UpdateOrDestroyShelterQuestion(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.ShelterQuestionSerializer
     permission_classes = [IsAuthenticated, permissions.IsAnyShelterOwner, permissions.IsShelterOwner]
 
     def get_object(self):
-        obj = get_object_or_404(ShelterQuestion, id=self.kwargs['question_id'], shelter_id=self.kwargs['pk'])
+        obj = get_object_or_404(ShelterQuestion, id=self.kwargs['question_id'])
         self.check_object_permissions(self.request, obj.shelter)
         return obj
