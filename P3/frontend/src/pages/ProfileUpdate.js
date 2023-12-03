648ec89088382c8c0fc4a7d92ce465961c8dbe5e
justinwli930 <justinwli930@gmail.com>
2023-12-03 15:59:56 -0500

    shelter profile update complete

--- P3/frontend/src/pages/ProfileUpdate.js
+++ P3/frontend/src/pages/ProfileUpdate.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
 import UserDetailService from '../services/UserDetailService';
 import NotificationPreferenceService from '../services/NotificationPreferenceService';
+import ShelterService from '../services/ShelterService';
 
 const ProfileUpdate = () => { 
     const { user, setUser } = useUser();
@@ -15,6 +16,7 @@ const ProfileUpdate = () => {
     const [notificationPreferences, setNotificationPreferences] = useState({})
     const [notificationUpdateNotification, setNotificationUpdateNotification] = useState('')
     const [shelter, setShelter] = useState({})
+    const [shelterNotification, setShelterNotification] = useState('')
     
     const handleUserProfileSubmit = async (values, { setSubmitting }) => {
         try {
@@ -53,8 +55,19 @@ const ProfileUpdate = () => {
     }, [])
 
     useEffect(() => {
-        
-    })
+        const getShelter = async () => {
+            if (user.is_shelter) {
+                try {
+                    const response = await ShelterService.getById(user.shelter_id);
+                    setShelter(response.data);
+                }
+                catch (error) {
+                    console.log('Error getting shelter', error);
+                }
+            }
+        }
+        getShelter();
+    }, [])
 
     const handleNotificationPreferenceSubmit = async (values, { setSubmitting }) => {
         try {
@@ -75,6 +88,30 @@ const ProfileUpdate = () => {
         }
         finally {
             setSubmitting(false)
+        }
+    }
+
+    const handleShelterProfileSubmit = async (values, { setSubmitting }) => {
+        if (user.is_shelter) {
+            try {
+                const response = await ShelterService.patch(user.shelter_id, values);
+                setShelter(response.data);
+                setShelterNotification('Shelter information updated successfully!');
+
+                setTimeout(() => {
+                    setShelterNotification('');
+                }, 5000)
+            }
+            catch (error) {
+                setShelterNotification('Error updating shelter information');
+
+                setTimeout(() => {
+                    setShelterNotification('');
+                }, 5000);
+            }
+            finally {
+                setSubmitting(false)
+            }
         }
     }
     
@@ -187,7 +224,87 @@ const ProfileUpdate = () => {
                     </div>
                 </div>
                 <div class="bg-white p-6 rounded pt-4 lg:w-3/4">
-                    {/* show shelter profile if applicable, else nothing */}
+                    {user.is_shelter ? (
+                        <>
+                            <Heading>Edit Shelter Profile</Heading>
+                            <Formik
+                                initialValues={{
+                                    shelter_name: shelter.shelter_name,
+                                    contact_email: shelter.contact_email,
+                                    location: shelter.location,
+                                    mission_statement: shelter.mission_statement
+                                }}
+                                onSubmit={handleShelterProfileSubmit}
+                                enableReinitialize>
+                                {({ isSubmitting, errors, isValid }) => (
+                                    <Form>
+                                        <div className="flex flex-col justify-start items-start w-full mb-4">
+                                            {shelterNotification && <Text color='text-[#ff9447]'>{shelterNotification}</Text>}
+
+                                            <label for="shelter_name" class="mt-3 block font-semibold">Shelter Name</label>
+                                            <TextInput 
+                                                id='shelter_name' 
+                                                name='shelter_name' 
+                                                placeholder='Shelter Name' 
+                                                className='lg:w-[22rem] w-[18rem] border-2 border-[#290005] bg-[#FFF8F4]' 
+                                                validate={(value) => {
+                                                    if (!value) {
+                                                        return 'This field is required'
+                                                    }}
+                                                }/>
+                                            {errors.shelter_name && <Text color='text-red-500'>{errors.shelter_name}</Text>}
+                                            
+                                            <label for="contact_email" class="mt-6 block font-semibold">Contact Email</label>
+                                            <TextInput
+                                                id='contact_email'
+                                                name='contact_email'
+                                                placeholder='Contact Email'
+                                                className='lg:w-[22rem] w-[18rem] border-2 border-[#290005] bg-[#FFF8F4]'
+                                                validate={(value) => {
+                                                    if (!value) {
+                                                        return 'This field is required'
+                                                    }
+                                                    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                                                        return 'Invalid email';
+                                                    }
+                                            }}/>
+                                            {errors.contact_email && <Text color='text-red-500'>{errors.contact_email}</Text>}
+
+                                            <label for="location" class="mt-6 block font-semibold">Location</label>
+                                            <TextInput
+                                                id='location'
+                                                name='location'
+                                                placeholder='Location'
+                                                className='lg:w-[22rem] w-[18rem] border-2 border-[#290005] bg-[#FFF8F4]'
+                                                validate={(value) => {
+                                                    if (!value) {
+                                                        return 'This field is required'
+                                                    }
+                                            }}/>
+                                            {errors.location && <Text color='text-red-500'>{errors.location}</Text>}
+
+                                            <label for="mission_statement" class="mt-6 block font-semibold">Mission Statement</label>
+                                            <TextInput
+                                                id='mission_statement'
+                                                name='mission_statement'
+                                                placeholder='Mission Statement'
+                                                className='lg:w-[22rem] w-[18rem] border-2 border-[#290005] bg-[#FFF8F4]'
+                                                validate={(value) => {
+                                                    if (!value) {
+                                                        return 'This field is required'
+                                                    }
+                                            }}/>
+                                            {errors.mission_statement && <Text color='text-red-500'>{errors.mission_statement}</Text>}
+
+                                            <Button className='mt-6' type='submit' disabled={isSubmitting || !isValid}>Submit</Button>
+                                        </div>
+                                    </Form>
+                                )}
+                            </Formik>
+                        </>
+                    ) : (
+                        null
+                    )}
                 </div>
             </div>
         </div>
