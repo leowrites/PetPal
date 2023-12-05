e54db7a6dca53f9e9894cbf6cdc199917fea2d68
justinwli930 <justinwli930@gmail.com>
2023-12-05 04:28:35 -0500

    BE validation for age

--- P3/backend/listings/models.py
+++ P3/backend/listings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-
+from django.core.validators import MaxValueValidator
 
 class PetListing(models.Model):
     STATUS_CHOICES = (("available", "Available"), ("not_available", "Not Available"))
@@ -8,7 +8,7 @@ class PetListing(models.Model):
     shelter = models.ForeignKey('shelters.Shelter', related_name="listings", on_delete=models.CASCADE)
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default="available")
     breed = models.CharField(max_length=200)
-    age = models.PositiveSmallIntegerField()
+    age = models.PositiveSmallIntegerField(validators=[MaxValueValidator(20)])
     image = CloudinaryField('image')
     image2 = CloudinaryField('image')
     image3 = CloudinaryField('image')
