fcf320ae504de988485e8227637277312952da66
justinwli930 <justinwli930@gmail.com>
2023-12-05 04:27:35 -0500

    FE age validation

--- P3/frontend/src/pages/NewListing.jsx
+++ P3/frontend/src/pages/NewListing.jsx
@@ -23,6 +23,8 @@ const NewListing = () => {
             name: 'age',
             displayName: 'Age',
             inputType: 'number',
+            min: 0,
+            max: 20,
             colSpan: 1,
             required: true,
         },
@@ -154,9 +156,14 @@ const NewListing = () => {
                                                 placeholder={field.displayName}
                                                 className='w-full'
                                                 type={field.inputType}
+                                                min = {field.min}
+                                                max = {field.max}
                                                 validate={(value) => {
                                                     if (!value && field.required) {
                                                         return 'This field is required'
+                                                    }
+                                                    if (field.inputType === 'number' && (value < 0 || value > 20)) {
+                                                        return 'Invalid age'
                                                     }
                                                 }}
                                             />
