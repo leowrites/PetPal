8e9948c5910454ecbf354e823291913551cf1e3e
leowrites <leo@liuwork.ca>
2023-12-06 14:23:42 -0500

    remove date format requirement

--- P3/backend/shelters/serializers/serializers.py
+++ P3/backend/shelters/serializers/serializers.py
@@ -56,7 +56,7 @@ def type_to_field(question_type, label, required):
     elif question_type == ShelterQuestion.CHECKBOX:
         return serializers.BooleanField(label=label, required=required)
     elif question_type == ShelterQuestion.DATE:
-        return serializers.DateField(input_formats=['%d-%m-%Y'], label=label, required=required)
+        return serializers.DateField(label=label, required=required)
     elif question_type == ShelterQuestion.EMAIL:
         return serializers.EmailField(max_length=200, min_length=0, label=label, required=required)
     elif question_type == ShelterQuestion.TEXT:
