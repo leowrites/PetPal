7d747870d5371a879e9530b339507723af976985
justinwli930 <justinwli930@gmail.com>
2023-12-06 00:11:15 -0500

    fix static

--- P3/backend/petpal/settings.py
+++ P3/backend/petpal/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'rest_framework',
     'shelters',
     'rest_framework_simplejwt',
@@ -155,6 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
