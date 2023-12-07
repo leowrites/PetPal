01a17ce5ea97f32c7cbdcd351c01a7dc07e9790c
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-12-06 20:27:53 -0500

    add: load next pages of messages

--- P3/frontend/src/pages/Message.jsx
+++ P3/frontend/src/pages/Message.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import Page from '../components/layout/Page';
 import Container from '../components/layout/Container';
 import { Input, Button } from '@material-tailwind/react'
-import { FaPaperPlane } from 'react-icons/fa';
+import { FaArrowUp, FaPaperPlane } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
 import PetApplicationCommentService from '../services/PetApplicationCommentService';
 import { useUser } from '../contexts/UserContext';
@@ -14,40 +14,81 @@ const Message = () => {
     const { applicationId } = useParams();
     const { user } = useUser();
     const divRef = useRef(null);
+    const [totalMessages, setTotalMessages] = useState(0);
+    const [currPage, setCurrPage] = useState(1);
 
     useEffect(() => {
         PetApplicationCommentService.list(applicationId)
             .then((res) => {
                 setMessages(res.data.results);
+                setTotalMessages(res.data.count);
+                scrollToBottom();
             })
             .catch((err) => {
                 console.log(err);
             });
     }, [applicationId])
 
-    const handleSendMessage = () => {
+    const handleSendMessage = (e) => {
+        e.preventDefault()
         PetApplicationCommentService.create(applicationId, message)
             .then((res) => {
                 setMessages([...messages, res.data]);
                 setMessage('');
+                setTotalMessages(totalMessages + 1);
             })
             .catch((err) => {
                 console.log(err);
             });
     }
 
-    useEffect(() => {
+    const scrollToBottom = () => {
         if (divRef.current) {
-        divRef.current.scrollTop = divRef.current.scrollHeight
+            divRef.current.scrollTop = divRef.current.scrollHeight
         }
+    }
+
+    const scrollToTop = () => {
+        if (divRef.current) {
+            divRef.current.scrollTop = 0
+        }
+    }
+
+    useEffect(() => {
+        scrollToBottom()
     }, [messages]);
+
+    useEffect(() => {
+        scrollToTop()
+    }, [currPage]);
+
+    const loadMoreMessages = () => {
+        PetApplicationCommentService.list(applicationId, currPage + 1)
+            .then((res) => {
+                setMessages([...messages, ...res.data.results]);
+                setCurrPage(currPage + 1);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
     return (
         <div className="min-h-screen w-full left-0 fixed">
             <div ref={divRef} className="lg:px-48 px-8 flex flex-col gap-1 overflow-auto h-[80vh] pb-36" style={{overflowAnchor: 'auto'}}>
                 {
-                    messages.sort((a, b) => (new Date(a.date_created)) > (new Date(b.date_created)) ? 1 : -1).map((message) => (
-                        <div className="flex flex-col gap-0">
+                    messages.length < totalMessages && (
+                        <div className="flex flex-row gap-2 items-baseline w-full justify-center">
+                            <Button className="rounded-full w-64 flex flex-row items-center justify-center gap-4 mb-4" size="sm" onClick={loadMoreMessages}>
+                                <FaArrowUp />
+                                Load Earlier Messages
+                            </Button>
+                        </div>
+                    )
+                }
+                {
+                    messages.sort((a, b) => (new Date(a.date_created)) > (new Date(b.date_created)) ? 1 : -1).map((message, i) => (
+                        <div className="flex flex-col gap-0" key={i}>
                             {/* {
                                 message.user !== user.id && (
                                     <p className="pl-4">{message.user}</p>
@@ -78,7 +119,11 @@ const Message = () => {
                 }
             </div>
             <div className="fixed bottom-20 w-full lg:px-48 px-8">
-                <div className="flex flex-row gap-2 items-baseline w-full">
+                <form
+                    noValidate
+                    className="flex flex-row gap-2 items-baseline w-full"
+                    onSubmit={handleSendMessage}
+                >
                     <input
                         type="text"
                         label="Send message"
@@ -86,7 +131,7 @@ const Message = () => {
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
                         className="rounded-full w-full px-6 py-2 border-2 border-gray-800"
-                        containerProps={{
+                        containerprops={{
                         className: "min-w-0",
                         }}
                     />
@@ -94,11 +139,12 @@ const Message = () => {
                     <Button
                         className="flex items-center gap-3 rounded-full px-4 py-3.5"
                         onClick={handleSendMessage}
+                        type='submit'
                     >
                         <FaPaperPlane />
                         Send
                     </Button>
-                </div>
+                </form>
             </div>
         </div>
     );
