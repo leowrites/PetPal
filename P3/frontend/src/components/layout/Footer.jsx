81c5b01e2662e07bc4906789cdb98df7843578dc
Pierre-William Lessard <43834016+PierreLessard@users.noreply.github.com>
2023-12-12 12:50:50 -0500

    Update Footer.jsx (#294)

--- P3/frontend/src/components/layout/Footer.jsx
+++ P3/frontend/src/components/layout/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
-        <footer className="absolute bottom-0 h-[3rem] bg-[#290005] text-center w-[100%] text-white py-[1rem] text-[1rem]">
-            © PetPal 2023
+        <footer className="absolute bottom-0 h-[3rem] bg-[#290005] text-center w-[100%] text-white py-[1rem] text-[1rem] flex flex-row">
+            © PetPal 2023 
+            <a href="https://github.com/leowrites/PetPal"> Github</a>
         </footer>
     )
 }
