6ea29cc1d42a1d2a6d10dbbbea95bcb830114128
leowrites <leo@liuwork.ca>
2023-12-12 20:09:54 -0500

    fix footer and env

--- P3/frontend/src/components/layout/Footer.jsx
+++ P3/frontend/src/components/layout/Footer.jsx
@@ -1,8 +1,10 @@
 export default function Footer() {
     return (
-        <footer className="absolute bottom-0 h-[3rem] bg-[#290005] text-center w-[100%] text-white py-[1rem] text-[1rem] flex flex-row justify-center">
+        <footer className="absolute bottom-0 h-[3rem] bg-[#290005] text-center w-[100%] text-white py-[1rem] text-[1rem] flex flex-row justify-center px-8 content-center">
+            <p>
             © PetPal 2023 
-            <a href="https://github.com/leowrites/PetPal"> Github</a>
+            </p>
+            <a className="ml-8" href="https://github.com/leowrites/PetPal"> Github</a>
         </footer>
     )
 }
